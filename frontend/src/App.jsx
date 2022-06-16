7dd88b995371f2190a6b8e365b196b8472b61fad
Quique Supertramp <quiquesupertramp@gmail.com>
2022-06-16 20:35:57 +0200

    skeleton frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+	return (
+		<div className='App'>
+			<h1>ESQUELETO VITE + REACT</h1>
+			<ul>
+				<li>ESLINT</li>
+				<li>PRETTIER</li>
+				<li>MODERN NORMALIZE</li>
+				<li>IMPORTS ABSOLUTOS CON @</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
