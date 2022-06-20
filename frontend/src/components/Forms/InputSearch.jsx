2ddd4354fbd7ebe298a47c8b509a0228fa59a452
Quique Supertramp <quiquesupertramp@gmail.com>
2022-06-20 13:13:21 +0200

    InputSearch created

--- frontend/src/components/Forms/InputSearch.jsx
+++ frontend/src/components/Forms/InputSearch.jsx
@@ -1,15 +1,11 @@
+import SearchIcon from '../Icons/SearchIcon';
 import style from './InputSearch.module.css';
 
-const InputSearch = ({ placeholder, error, ...props }) => {
+const InputSearch = ({ className = '', ...props }) => {
 	return (
-		<div className={style.inputSearch}>
-			<input
-				{...props}
-				className={style.input}
-				type='text'
-				placeholder={placeholder}
-			/>
-			{error && <span className={style.error}>* Error</span>}
+		<div className={style.inputContainer}>
+			<SearchIcon className={style.icon} />
+			<input {...props} className={style.input} type='text' />
 		</div>
 	);
 };
