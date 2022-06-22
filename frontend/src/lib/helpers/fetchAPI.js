48b4aa885cd88521831f9dc64b84c001cba310b9
Quique Supertramp <quiquesupertramp@gmail.com>
2022-06-22 04:44:47 +0200

    refactor api services

---
+++ frontend/src/lib/helpers/fetchAPI.js
@@ -0,0 +1,34 @@
+// Functions
+const URL_API = 'https://userslistpractice.herokuapp.com/users';
+
+const fetchAPI = async ({ url, method, signal, body }) => {
+	try {
+		const res = await fetchAction({ url, method, signal, body });
+		if (!res.ok) throw new Error();
+		const data = await res.json();
+		return { data, error: false, aborted: false };
+	} catch (error) {
+		if (error.name === 'AbortError')
+			return {
+				data: null,
+				error: false,
+				aborted: 'La petición ha sido cancelada',
+			};
+		return {
+			data: null,
+			error,
+			aborted: false,
+		};
+	}
+};
+
+const fetchAction = async ({ url, method, signal, body }) => {
+	return await fetch(url || URL_API, {
+		method: method || 'GET',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(body),
+		signal,
+	});
+};
+
+export default fetchAPI;
--- frontend/src/lib/services/api.js
+++ frontend/src/lib/services/api.js
@@ -1,14 +1,17 @@
-export const fetchUsers = async signal => {
-	try {
-		const res = await fetch('https://userslistpractice.herokuapp.com/users', {
-			signal,
-		});
-		if (!res.ok) throw new Error();
-		const users = await res.json();
-		return { users, error: false, aborted: false };
-	} catch (error) {
-		if (error.name === 'AbortError')
-			return { users: null, error: false, aborted: true };
-		return { users: null, error: true, aborted: false };
-	}
-};
+import fetchAPI from '../helpers/fetchAction';
+
+// fetchAPI possible props => { url = URL_API, method = 'GET, signal, body }
+// fetchAPI structure res => { data, error, aborted }
+
+export const getUsers = async signal => await fetchAPI({ signal });
+
+export const getUserById = async url => await fetchAPI({ url });
+
+export const createUser = async newUser =>
+	await fetchAPI({ method: 'POST', body: newUser });
+
+export const deleteUser = async url =>
+	await fetchAPI({ url, method: 'DELETE' });
+
+export const updateUser = async (url, userChanges) =>
+	await fetchAPI({ url, body: userChanges, method: 'PATCH' });
