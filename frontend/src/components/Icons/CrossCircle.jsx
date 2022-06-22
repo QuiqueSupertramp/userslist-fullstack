0d6c24763807561942353716a9dee3038803a805
Quique Supertramp <quiquesupertramp@gmail.com>
2022-06-22 17:06:57 +0200

    new icons

--- frontend/src/components/Icons/CrossCircle.jsx
+++ frontend/src/components/Icons/CrossCircle.jsx
@@ -1,15 +1,15 @@
 const CrossCircle = props => (
 	<svg
+		{...props}
 		fill='none'
 		stroke='currentColor'
 		viewBox='0 0 24 24'
-		xmlns='http://www.w3.org/2000/svg'
-		{...props}>
+		xmlns='http://www.w3.org/2000/svg'>
 		<path
 			strokeLinecap='round'
 			strokeLinejoin='round'
 			strokeWidth='2'
-			d='M6 18L18 6M6 6l12 12'></path>
+			d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'></path>
 	</svg>
 );
 
