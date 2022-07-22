555f7d4c3821389be0895959ccb50f055986e288
Quique Supertramp <quiquesupertramp@gmail.com>
2022-07-22 14:54:19 +0200

    header icon theme

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
+import Button from '../Buttons/Button';
+import MoonIcon from '../Icons/MoonIcon';
+import SunIcon from '../Icons/SunIcon';
+import style from './Header.module.css';
+
 const Header = ({ theme, setTheme }) => {
+	const Icon = theme === 'dark' ? SunIcon : MoonIcon;
 	return (
-		<header className='header'>
-			<div className='headerContainer'>
-				<div className='headerNav'>
-					<h1 className='title'>Listado de usuarios</h1>
-					<button
+		<header className={style.header}>
+			<div className={style.headerContainer}>
+				<div className={style.headerNav}>
+					<h1 className={style.headerTitle}>Listado de usuarios</h1>
+					<Button
+						className={style.header__btn}
 						onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+						<Icon className={style.header__icon} />
 						{theme === 'dark' ? 'light' : 'dark'}
-					</button>
+					</Button>
 				</div>
 			</div>
 		</header>
